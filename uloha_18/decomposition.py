3866c498ff29c5a821ef6d1042f8b0ac518a3c82
Václav Thám <venca275tham@gmail.com>
2022-02-10 00:08:14 +0100

    Zapracované připomínky k úloze 18

--- uloha_18/decomposition.py
+++ uloha_18/decomposition.py
@@ -1,42 +1,39 @@
-# function that decomposes an input parameter n (a natural number greater than 1) into the product of prime numbers
+import math
+# Function that decomposes an input parameter n (a natural number greater than 1) into the product of prime numbers
 def decomp_into_prime_num(n):
+    # Verify that the input value is correct if the input is not correct program ends.
     if n <= 1:
-        exit("CHYBA: Prvočíselný rozklad lze provést pouze pro číslo N > 1")
+        exit("ERROR: Prime decomposition can only be performed for number N> 1")
     if n % 1 != 0:
-        exit("CHYBA: Prvočíselný rozklad lze provést pouze pro přirozené číslo N")
-    
-    print("Prvočíselný rozklad čísla {} je:".format(n))
+        exit("ERROR: Prime decomposition can only be performed for the natural number N")  
+    print("Prime number decomposition of a number {} is:".format(n))
     m = n
-    num = 0
     exp = 0
     prime = 2
-
+# The cycle of prime decomposition
     while n > 1:
-        if num == 0:
-            num = prime
-            prime += 1
-        
-        if n % num == 0:
+        if n % prime == 0: # condition whether n is divisible by a prime number. It finds out if there's a same prime number once or twice
             exp += 1
-            n = n/num
-        else:
+            n = n/prime
+        else: # print values if there is a prime number only once or more
             if exp == 1:
-                print("{} x".format(num), end = " ")
+                print("{} x".format(prime), end = " ")
             if exp > 1:
-                print("{}^{} x".format(num, exp), end = " ")
-            
-            num = 0
+                print("{}^{} x".format(prime, exp), end = " ")
+            prime+=1
             exp = 0
-    if num == m:
-        print("Číslo {} je prvočíslo".format(m))
+    # Condition if the input value is a prime number
+    if prime == m:
+        print("Number {} is prime number".format(m))
     else:
         if exp == 1:
-            print(num)
+            print(prime)
         if exp > 1:
-            print("{}^{}".format(num, exp))
-        
+            print("{}^{}".format(prime, exp))
+
 # run function
-NATURAL_NUMBER= 100
+NATURAL_NUMBER = 100
+# Verify that the input value is not integer, if its not the program ends.
 if type(NATURAL_NUMBER) != int:
-    exit("CHYBA: Prvočíselný rozklad lze provést pouze pro přirozené číslo N")
+    exit("ERROR: Prime decomposition can only be performed for the natural number N")
 decomp_into_prime_num(NATURAL_NUMBER)
