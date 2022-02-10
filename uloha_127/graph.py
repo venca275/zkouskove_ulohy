f238ffab857eb11f1991895c94ba9d41744428ef
Václav Thám <venca275tham@gmail.com>
2022-02-10 13:29:29 +0100

    Z třídy graf odstraněna datová položka first_vertex

--- uloha_127/graph.py
+++ uloha_127/graph.py
@@ -1,18 +1,16 @@
+from cProfile import label
 from vertex import Vertex
 
 class Graph:
 
     def __init__(self, lines):
         self.vertices = {}
-        self.first_vertex = None
-
+        # Cycle that goes through all the lines in the file
         for line in lines:
             [label1, label2] = line.split(" ")
-            if label1 not in self.vertices: # přidá label1 do seznamu ve třídě Vertex, pokud tam ještě není
+            if label1 not in self.vertices: # adds label1 to the list in the Vertex class if it is not already there
                 self.vertices[label1] = Vertex(label1)
-                if self.first_vertex == None:
-                    self.first_vertex = self.vertices[label1]
-            if label2 not in self.vertices: # přidá label2 do seznamu ve třídě Vertex, pokud tam ještě není
+            if label2 not in self.vertices: # adds label2 to the list in the Vertex class if it is not already there
                 self.vertices[label2] = Vertex(label2)
             self.vertices[label1].add_neighbour(self.vertices[label2])
             self.vertices[label2].add_neighbour(self.vertices[label1])
